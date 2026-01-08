627f900c8d6bb8953d496c8f91bb8ed3c980ac30
Nthiaa17 <dialloanthia03@gmail.com>
2026-01-08 21:14:14 +0100

    Create Eleve.java
    Creation classe Eleve

---
+++ src/main/java/Domain/Models/Eleve.java
@@ -0,0 +1,42 @@
+package Domain.Models;
+
+public class Eleve extends Utilisateur{
+    private int age;
+    private String niveauEtude;
+
+    public Eleve() {
+        super();
+    }
+
+    public Eleve(String nom, String prenom, String email, String motDePasseHash,
+                 int age, String niveauEtude) {
+        super(nom, prenom, email, motDePasseHash);
+        this.age = age;
+        this.niveauEtude = niveauEtude;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNiveauEtude() {
+        return niveauEtude;
+    }
+    public void setNiveauEtude(String niveauEtude) {
+        this.niveauEtude = niveauEtude;
+    }
+
+    @Override
+    public String toString() {
+        return "Eleve{" +
+                "id=" + getId() +
+                ", age=" + age +
+                ", niveauEtude='" + niveauEtude + '\'' +
+                '}';
+    }
+}
+
+
