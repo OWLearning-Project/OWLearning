4fbe42990727a7d24ea598a043b09b50b9d05ce4
BryoX95 <bryan.biglione@icloud.com>
2026-01-13 18:15:56 +0100

    modification des classes Messages et Ressources sur les getter et setter

--- src/main/java/Domain/Models/Message.java
+++ src/main/java/Domain/Models/Message.java
@@ -2,7 +2,9 @@ package Domain.Models;
 
 import jakarta.persistence.*;
 
+import java.sql.Array;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 @Entity
 public class Message {
@@ -26,6 +28,10 @@ public class Message {
     @JoinColumn(name = "id_utilisateur")
     private Utilisateur utilisateur;
 
+    @OneToMany
+    @JoinColumn(name = "id_message")
+    private ArrayList<Ressource> ressources = new ArrayList<>();
+
     public Message() {
     }
 
@@ -37,15 +43,27 @@ public class Message {
 
 
     public int getId_message() {
-        return id_message;
+        return this.id_message;
     }
 
     public Timestamp getDate_creation() {
-        return date_creation;
+        return this.date_creation;
     }
 
     public String getContenu() {
-        return contenu;
+        return this.contenu;
+    }
+
+    public ArrayList<Ressource> getRessources(){
+        return this.ressources;
+    }
+
+    public Discussion getDiscussion(){
+        return this.discussion;
+    }
+
+    public Utilisateur getUtilisateur(){
+        return this.utilisateur;
     }
 
     public void setId_message(int id_message) {
@@ -59,5 +77,21 @@ public class Message {
     public void setContenu(String contenu) {
         this.contenu = contenu;
     }
+
+    public void setRessources(ArrayList<Ressource> desRessources){
+        this.ressources = desRessources;
+    }
+
+    public void setDiscussion(Discussion uneDiscussion) {
+        this.discussion = uneDiscussion;
+    }
+
+    public void setUtilisateur(Utilisateur unUtilisateur) {
+        this.utilisateur = unUtilisateur;
+    }
+
+    public void ajouterRessource(Ressource uneRessource){}
+
+    public Ressource retirerRessource(int uneRessourceId){return null;}
 
 }
