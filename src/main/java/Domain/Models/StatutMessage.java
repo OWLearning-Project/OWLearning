83dc8fc6d2a61fa26413e9a31777a6ee008c9773
BryoX95 <bryan.biglione@icloud.com>
2026-01-13 22:10:35 +0100

    Transformation des Enum StatutMessage et TypeRessource en classe

--- src/main/java/Domain/Models/StatutMessage.java
+++ src/main/java/Domain/Models/StatutMessage.java
@@ -1,6 +1,28 @@
 package Domain.Models;
 
-public enum StatutMessage {
-    Envoi,
-    Envoye
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class StatutMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_statutMessage;
+    private String statutMessage;
+
+    public StatutMessage(){}
+
+    public StatutMessage(String unStatutMessage){
+        this.statutMessage = unStatutMessage;
+    }
+
+    public int getIdStatutMessage(){
+        return this.id_statutMessage;
+    }
+    public String getStatutMessage(){
+        return this.statutMessage;
+    }
 }
--- src/main/java/Domain/Models/TypeRessource.java
+++ src/main/java/Domain/Models/TypeRessource.java
@@ -1,9 +1,28 @@
 package Domain.Models;
 
-public enum TypeRessource {
-    Video,
-    FichierPdf,
-    FichierZip,
-    Image
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class TypeRessource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_typeRessource;
+    private String typeRessource;
+
+    public TypeRessource(){}
+
+    public TypeRessource(String unTypeMessage){
+        this.typeRessource = unTypeMessage;
+    }
+
+    public int getIdTypeRessource(){
+        return this.id_typeRessource;
+    }
+    public String getTypeRessource(){
+        return this.typeRessource;
+    }
 }
