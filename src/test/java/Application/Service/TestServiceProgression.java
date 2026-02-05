d316c1defd51d87decec610ea4f38d2705756268
BryoX95 <bryan.biglione@icloud.com>
2026-02-05 16:21:01 +0100

    Green test pour toutes les méthodes du service progression

--- src/test/java/Application/Service/TestServiceProgression.java
+++ src/test/java/Application/Service/TestServiceProgression.java
@@ -1,6 +1,7 @@
 package Application.Service;
 
 import Application.Services.ServiceProgression;
+import Domain.Models.Chapitre;
 import Domain.Models.Cours;
 import Domain.Models.Eleve;
 import Domain.Models.Progression;
@@ -14,8 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TestServiceProgression {
@@ -105,6 +105,51 @@ public class TestServiceProgression {
 
     @Test
     public void TestMajProgression(){
+        //Arrange
+        int eleveId = 1;
+        int coursId = 10;
+        int chapitreId = 2;
 
+        Eleve eleveTest = mock(Eleve.class);
+        Cours coursTest = mock(Cours.class);
+
+        ArrayList<Chapitre> listeChapitres = new ArrayList<>();
+        listeChapitres.add(mock(Chapitre.class));
+        listeChapitres.add(mock(Chapitre.class));
+        listeChapitres.add(mock(Chapitre.class));
+        listeChapitres.add(mock(Chapitre.class));
+        when(coursTest.getChapitres()).thenReturn(listeChapitres);
+
+        Progression progressionExistante = new Progression(coursTest, eleveTest);
+        progressionExistante.setTauxProgression(25.0f);
+
+        when(repositoryTest.trouverParId(eleveId, coursId)).thenReturn(progressionExistante);
+
+        //Act
+        serviceTester.majProgression(eleveId, coursId, chapitreId);
+
+        //Assert
+        assertEquals(50.0f, progressionExistante.getTauxProgression());
+        verify(repositoryTest).maj(progressionExistante);
+    }
+
+    @Test
+    public void TestRecuperationProgression(){
+        //Arrange
+        Eleve eleveTest = mock(Eleve.class);
+        int eleveId = 1;
+        Cours coursTest = mock(Cours.class);
+        int coursId = 9;
+
+        Progression progressionTest = new Progression(coursTest, eleveTest);
+        progressionTest.setTauxProgression(75.0f);
+
+        when(repositoryTest.trouverParId(eleveId, coursId)).thenReturn(progressionTest);
+
+        //Act
+        float resultat = serviceTester.getProgressionEleve(eleveId, coursId);
+
+        //Assert
+        assertEquals(75.0f, resultat);
     }
 }
