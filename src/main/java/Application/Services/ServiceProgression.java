86a73125b5d313d986dfbbfd5ec89149f677feef
BryoX95 <bryan.biglione@icloud.com>
2026-02-05 16:21:01 +0100

    Green test pour toutes les méthodes du service progression

--- src/main/java/Application/Services/ServiceProgression.java
+++ src/main/java/Application/Services/ServiceProgression.java
@@ -1,9 +1,11 @@
 package Application.Services;
 
+import Domain.Models.Chapitre;
 import Domain.Models.Cours;
 import Domain.Models.Progression;
 import Domain.Ports.IRepository.IProgressionRepository;
 import Domain.Ports.IServices.IServiceProgression;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,11 +48,32 @@ public class ServiceProgression implements IServiceProgression {
 
     @Override
     public void majProgression(int elevId, int coursId, int chapitreId) {
+        Progression progressionExistante = progressionRepository.trouverParId(elevId, coursId);
+        //vérifie si l'élève est inscrit
+        if(progressionExistante == null){
+            throw new EntityNotFoundException("L'élève n'est pas inscrit au cours");
+        }
 
+        ArrayList<Chapitre> nb_chapitres = progressionExistante.getCours().getChapitres();
+
+        //Pour ne pas diviser par 0
+        if(nb_chapitres != null && !nb_chapitres.isEmpty()){
+            int nb_chapitre_total = progressionExistante.getCours().getChapitres().size();
+            float nouvelleProgression = ((float) chapitreId / nb_chapitre_total)*100;
+
+            progressionExistante.setTauxProgression(nouvelleProgression);
+            progressionRepository.maj(progressionExistante);
+        }
     }
 
     @Override
     public float getProgressionEleve(int eleveId, int coursId) {
-        return 0;
+        Progression progression = progressionRepository.trouverParId(eleveId, coursId);
+        if(progression == null){
+            return 0.0f;
+        }
+        else{
+            return progression.getTauxProgression();
+        }
     }
 }
