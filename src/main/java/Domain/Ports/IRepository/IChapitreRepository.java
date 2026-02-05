67f1abf343ec4d23a3eb2c1a273aa84605877a56
b0uman <narutovignon@gmail.com>
2026-02-05 12:28:39 +0100

    ajout de nouvelle fonction dans interface repo chapitre

--- src/main/java/Domain/Ports/IRepository/IChapitreRepository.java
+++ src/main/java/Domain/Ports/IRepository/IChapitreRepository.java
@@ -1,10 +1,9 @@
 package Domain.Ports.IRepository;
 
 import Domain.Models.Chapitre;
+import Domain.Models.Ressource;
 
-import java.util.List;
-
-public interface ChapitreRepository {
+public interface IChapitreRepository {
 
     /**
      * Sauvegarde un chapitre dans la bd
@@ -21,10 +20,30 @@ public interface ChapitreRepository {
     public Chapitre trouverParId(int i);
 
     /**
-     * Recherche tout les chapitres de la bd
-     * @return list des chapitres, ou null si pas de chapitre trouvé.
+     * Permet de changer de titre ou de description pour un chapitre
+     * @param chapitre à modifier
      */
-    public List<Chapitre> trouverTout();
+    public void modifier(Chapitre chapitre);
+
+    /**
+     * Ajout une ressource au chapitre selectionné
+     * @param id du chapitre
+     * @param ressource
+     */
+    public void ajouterRessource(int id, Ressource ressource);
+
+    /**
+     * Supprime la ressource de tout les chapitres
+     * @param id
+     */
+    public void supprimerRessource(int id);
+
+    /**
+     * Détache la ressource du chapitre
+     * @param idChapitre
+     * @param idRessource
+     */
+    public void retirerRessourceDuChapitre(int idChapitre, int idRessource);
 
     /**
      * supprimé le chapitre
@@ -34,7 +53,7 @@ public interface ChapitreRepository {
     public Chapitre supprimerParId(int i);
 
     /**
-     * verification
+     * verification de l'existance du chapitre
      * @param i id du chapitre
      * @return True si le chapitre existe, sinon false si il n'existe pas
      */
