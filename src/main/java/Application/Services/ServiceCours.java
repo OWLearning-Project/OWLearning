94adef1d52225bd6b21f43ee18ab569ac19e7975
Noémie <noemiechhun@gmail.com>
2026-02-06 17:15:07 +0100

    Corrections sur le service et repo de Cours

--- src/main/java/Application/Services/ServiceCours.java
+++ src/main/java/Application/Services/ServiceCours.java
@@ -2,48 +2,68 @@ package Application.Services;
 
 import Domain.Models.Cours;
 import Domain.Ports.IRepository.ICoursRepository;
+import Domain.Ports.IServices.IServiceCours;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Classe ServiceCours, permet de gérer le traitement des cours
+ */
 @Service
-public class ServiceCours
+public class ServiceCours implements IServiceCours
 {
     private final ICoursRepository coursRepository;
 
+    /**
+     * Constructeur du ServiceCours
+     * @param coursRepository
+     */
     public ServiceCours (ICoursRepository coursRepository)
     {
         this.coursRepository = coursRepository;
     }
 
-    public ArrayList<Cours> getTousLesCours()
-    {
-        List<Cours> listeCours = coursRepository.trouverTousLesCours();
-        return new ArrayList<Cours>(listeCours);
-    }
-
-    public ArrayList<Cours> getCoursParTitre(String titre)
-    {
-        List<Cours> listeCours = coursRepository.trouverParTitre(titre);
-        return new ArrayList<Cours>(listeCours);
-    }
-
+    /**
+     * Méthode permettant de récupérer un cours avec son id
+     * @param id id du cours
+     * @return l'objet Cours associé
+     */
     public Cours getCoursParId(int id)
     {
         return coursRepository.trouverParId(id);
     }
 
-    public ArrayList<Cours> getCoursPublies(int createurId)
+    /**
+     * Méthode qui permet de récupérer les cours créés par un créateur avec son id
+     * Filtrage possible avec les paramètres
+     * @param idCreateur id du créateur
+     * @return l'ArrayList des Cours créés
+     */
+    public ArrayList<Cours> getCoursCrees(String titre, int idCreateur, String difficulte, String categorie, Boolean estPrive)
     {
-        List<Cours> listeCours = coursRepository.trouverParIdCreateur(createurId);
-        return new ArrayList<Cours>(listeCours);
+        return coursRepository.trouverParIdCreateur(titre, idCreateur, difficulte, categorie, estPrive);
     }
 
-    public ArrayList<Cours> getCoursInscrits(int eleveId)
+    /**
+     * Méthode permettant de récupérer les Cours où un élève est inscrit avec son id
+     * Filtrage possible avec les paramètres
+     * @param eleveId id de l'élève
+     * @return l'ArrayList des Cours inscrits
+     */
+    public ArrayList<Cours> getCoursInscrits(int eleveId, String titre, String nomCreateur, String difficulte, String categorie, Boolean estPrive)
     {
-        List<Cours> listeCours = coursRepository.trouverParIdEleve(eleveId);
-        return new ArrayList<>(listeCours);
+        return coursRepository.trouverParIdEleve(eleveId, titre, nomCreateur, difficulte, categorie, estPrive);
     }
 
+    /**
+     * Méthode permettant de récupérer l'entièreté des Cours publiés
+     * Filtrage possible avec les paramètres
+     * @return l'ArrayList des Cours publiés
+     */
+    @Override
+    public ArrayList<Cours> getLesCours(String titre, String nomCreateur, String difficulte, String categorie, Boolean estPrive)
+    {
+        return coursRepository.trouverCoursFiltre(titre, nomCreateur, difficulte, categorie, estPrive);
+    }
 }
--- src/main/java/Domain/Ports/IRepository/ICoursRepository.java
+++ src/main/java/Domain/Ports/IRepository/ICoursRepository.java
@@ -2,17 +2,37 @@ package Domain.Ports.IRepository;
 
 import Domain.Models.Cours;
 
-import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Interface ICoursRepository définissant le contrat pour la récupération des cours
+ */
 public interface ICoursRepository
 {
-    public List<Cours> trouverTousLesCours();
+    /**
+     * Méthode pour trouver les cours publiés avec ou sans filtre
+     * @return l'ArrayList des Cours publiés
+     */
+    public abstract ArrayList<Cours> trouverCoursFiltre(String titre, String nomCreateur, String difficulte, String categorie, Boolean estPrive);
 
-    public List<Cours> trouverParTitre(String titre);
+    /**
+     * Méthode pour trouver un cours par son id
+     * @param id id du cours
+     * @return l'objet Cours associé
+     */
+    public abstract Cours trouverParId(int id);
 
-    public Cours trouverParId(int id);
+    /**
+     * Méthode pour trouver les cours créés par un créateur via son id
+     * @param createurId id du créateur
+     * @return l'ArrayList des cours créés
+     */
+    public abstract ArrayList<Cours> trouverParIdCreateur(String titre, int createurId, String difficulte, String categorie, Boolean estPrive);
 
-    public List<Cours> trouverParIdCreateur(int createurId);
-
-    public List<Cours> trouverParIdEleve(int eleveId);
+    /**
+     * Méthode pour trouver les cours où un élève est inscrit via son id
+     * @param eleveId id de l'élève
+     * @return l'ArrayList des cours inscrits
+     */
+    public abstract ArrayList<Cours> trouverParIdEleve(int eleveId, String titre, String nomCreateur, String difficulte, String categorie, Boolean estPrive);
 }
