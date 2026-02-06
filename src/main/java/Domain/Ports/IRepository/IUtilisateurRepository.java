850ae27659d82b11dfea596e7b46ebb6174c5059
Nthiaa17 <dialloanthia03@gmail.com>
2026-02-06 10:26:52 +0100

    Create IUtilisateurRepository.java
    interface Utilisateur + 2 méthodes ajoutées

---
+++ src/main/java/Domain/Ports/IRepository/IUtilisateurRepository.java
@@ -0,0 +1,32 @@
+package Domain.Ports.IRepository;
+
+import Domain.Models.Utilisateur;
+
+/**
+ * Interface de UtilisateurRepository qui permet de trouver un Utilisateur grâce à son email et de sauvegarder un Utilisateur
+ */
+public interface IUtilisateurRepository
+{
+    /**
+     * Méthode qui permet de trouver l'utilisateur via son email
+     * @param email
+     * @return l'Utilisateur trouvé
+     */
+    public Utilisateur trouverParEmail(String email);
+    // trouver le user par son Id
+    public Utilisateur trouverParId(int id);
+    /**
+     * Méthode qui permet de sauvegarder un Utilisateur
+     * @param utilisateur
+     * @return le nombre de ligne sauvegarder
+     */
+    public int sauvegarder(Utilisateur utilisateur);
+
+    public int sauvegarderCreateur(int id);
+
+    public int sauvegarderEleve(int id);
+
+    public int trouverIdParEmail(String email);
+
+    public int mettreAJour(Utilisateur utilisateur);
+}
