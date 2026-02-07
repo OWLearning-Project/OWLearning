2b6d57c3369056631f077e78aac100066fdfdd7f
BryoX95 <bryan.biglione@icloud.com>
2026-02-07 14:44:25 +0100

    suppression des methodes du service progression sauf le getProgressionEleve à la suite de la réunion

--- src/main/java/Domain/Ports/IServices/IServiceProgression.java
+++ src/main/java/Domain/Ports/IServices/IServiceProgression.java
@@ -5,12 +5,12 @@ import Domain.Models.Cours;
 import java.util.ArrayList;
 
 public interface IServiceProgression {
-
+/**
     public ArrayList<Cours> getCoursTermines(int eleveId);
 
     public ArrayList<Cours> getCoursPasTermines(int eleveId);
 
     public void majProgression(int elevId, int coursId, int chapitreId);
-
+**/
     public float getProgressionEleve(int eleveId, int coursId);
 }
