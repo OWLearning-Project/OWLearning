45deab58dfc35c423084995055ee22698d45b78d
b0uman <narutovignon@gmail.com>
2026-02-22 23:20:03 +0100

    debut controller

--- src/main/java/app/OwLearning/Api/Chapitre/ChapitreController.java
+++ src/main/java/app/OwLearning/Api/Chapitre/ChapitreController.java
@@ -1,9 +1,12 @@
 package app.OwLearning.Api.Chapitre;
 
 
+import app.OwLearning.Domain.Models.Chapitre;
 import app.OwLearning.Domain.Ports.IServices.IServiceChapitre;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("api/chapitre")
@@ -12,5 +15,15 @@ public class ChapitreController {
 
     public ChapitreController(IServiceChapitre serviceChapitre){
         this.serviceChapitre = serviceChapitre;
+    }
+
+    @GetMapping("/{idChapitre}")
+    public ResponseEntity<Chapitre> getChapitre(@PathVariable("idChapitre")int idChapitre){
+        return ResponseEntity.ok(this.serviceChapitre.getContenuChapitre(idChapitre));
+    }
+
+    @PutMapping("/{idChapitre}")
+    public ResponseEntity<Void> modifierChapitre(@PathVariable("idChapitre") int idChapitre, @RequestBody ChapitreDTO chapitreDTO){
+
     }
 }
