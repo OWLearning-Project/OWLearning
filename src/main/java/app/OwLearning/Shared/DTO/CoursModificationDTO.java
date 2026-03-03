1ca7a00fd4ee543f5bacd17aa6d310e49620047c
Nthiaa17 <dialloanthia03@gmail.com>
2026-03-03 22:09:57 +0100

    Create CoursModificationDTO.java
    DTO pour la modification d'un cours

---
+++ src/main/java/app/OwLearning/Shared/DTO/CoursModificationDTO.java
@@ -0,0 +1,9 @@
+package app.OwLearning.Shared.DTO;
+
+public class CoursModificationDTO {
+    private String titre;
+    private String description;
+
+    public String getTitre() { return titre; }
+    public String getDescription() { return description; }
+}
