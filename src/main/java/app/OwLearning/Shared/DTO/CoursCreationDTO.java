a09eb5ec89821cedaffb26f357046efd0ea8dc08
Nthiaa17 <dialloanthia03@gmail.com>
2026-03-03 22:09:29 +0100

    Create CoursCreationDTO.java
    DTO pour la creation d'un cours

---
+++ src/main/java/app/OwLearning/Shared/DTO/CoursCreationDTO.java
@@ -0,0 +1,12 @@
+package app.OwLearning.Shared.DTO;
+
+public class CoursCreationDTO {
+    private String titre;
+    private String description;
+    private String categorie;
+    private int createurId;
+
+    public String getTitre() { return titre; }
+    public String getDescription() { return description; }
+    public String getCategorie() { return categorie; }
+    public int getCreateurId() { return createurId; }}
