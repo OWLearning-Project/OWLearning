cf63f4e8c9502a92578c7235160805830af5e587
Iraihane Akretche <iraihane.akretche@outlook.de>
2026-03-06 22:49:16 +0100

    fix controller

--- src/main/java/app/OwLearning/Domain/Models/Chapitre.java
+++ src/main/java/app/OwLearning/Domain/Models/Chapitre.java
@@ -21,8 +21,11 @@ public class Chapitre
     @JoinColumn(name = "id_cours")
     private Cours cours;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "id_chapitre")
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name="ressource_chapitre",
+                joinColumns = @JoinColumn(name="id_chapitre"),
+                inverseJoinColumns = @JoinColumn(name="id_ressource")
+    )
     private List<Ressource> ressources;
     public Chapitre(){}
 
@@ -71,7 +74,7 @@ public class Chapitre
         return ressources;
     }
 
-    public void setRessources(ArrayList<Ressource> ressources) {
+    public void setRessources(List<Ressource> ressources) {
         this.ressources = ressources;
     }
 
---
+++ src/main/java/app/OwLearning/Shared/DTO/AjoutRessourceDTO.java
@@ -0,0 +1,39 @@
+package app.OwLearning.Shared.DTO;
+
+import app.OwLearning.Domain.Models.TypeRessource;
+
+public class AjoutRessourceDTO
+{
+    private String nom;
+    private String url;
+    private TypeRessource type;
+
+    public AjoutRessourceDTO() {}
+
+    public String getNom()
+    {
+        return nom;
+    }
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+    public void setUrl(String url)
+    {
+        this.url = url;
+    }
+
+    public TypeRessource getType()
+    {
+        return type;
+    }
+    public void setType(TypeRessource type)
+    {
+        this.type = type;
+    }
+}
