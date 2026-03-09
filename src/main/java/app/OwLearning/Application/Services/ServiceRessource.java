2f35b82f00ac7d74d2382f979adc732c5afc80d4
b0uman <narutovignon@gmail.com>
2026-03-09 20:01:17 +0100

    feat: ajout logs au fichier ServiceRessource

--- src/main/java/app/OwLearning/Application/Services/ServiceRessource.java
+++ src/main/java/app/OwLearning/Application/Services/ServiceRessource.java
@@ -7,8 +7,10 @@ import app.OwLearning.Domain.Ports.IServices.IServiceRessource;
 import app.OwLearning.Shared.Exceptions.ExceptionRessourceIntrouvable;
 import app.OwLearning.Shared.Exceptions.ExceptionRessourceIntrouvableDansChap;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ServiceRessource implements IServiceRessource {
 
@@ -20,38 +22,63 @@ public class ServiceRessource implements IServiceRessource {
 
     @Override
     public Ressource getContenuRessource(int id) {
+        log.debug("Demande de la récupération de l'id de la ressource: {}", id);
         Ressource ressource = this.repository.trouverParId(id);
 
         if (ressource == null) {
-            throw new ExceptionRessourceIntrouvable(id);
+            log.warn("La ressource {} est introuvable dans la base", id);
+            throw new ExceptionRessourceIntrouvable("La ressource est introuvable", id);
         }
+        log.debug("Ressource {} récupérée: {}", id, ressource);
         return ressource;
     }
 
     @Override
     public Ressource creeRessource(String nom, String url, TypeRessource type) {
-        if (nom == null || nom.isBlank()) throw new IllegalArgumentException("Le nom n'est pas valide");
-        if (url == null || url.isBlank()) throw new IllegalArgumentException("L'url n'est pas valide");
-        if (type == null) throw new IllegalArgumentException("Type de ressource non selectionné");
+        log.debug("Demande de création d'une ressource: nom='{}', url='{}', type='{}'", nom, url, type);
+        if (nom == null || nom.isBlank()){
+            log.warn("Echec de création de la ressource: le nom n'est pas disponible");
+            throw new IllegalArgumentException("Le nom n'est pas valide");
+        }
+        if (url == null || url.isBlank()){
+            log.error("Echec de création de la ressource: l'url n'est pas disponible");
+            throw new IllegalArgumentException("L'url n'est pas valide");
+        }
+        if (type == null){
+            log.error("Echec de création de la ressource: le type de ressource n'est pas disponible ");
+            throw new IllegalArgumentException("Type de ressource non selectionné");
+        }
 
-        return this.repository.sauvegarder(new Ressource(nom,type,url));
+        Ressource ressource = this.repository.sauvegarder(new Ressource(nom,type,url));
+
+        log.info("Création de ressource: {}", ressource);
+        return ressource;
     }
 
     @Override
     public Ressource supprimerRessource(int id) {
+        log.debug("Demande de suppression de la ressource (id): {}", id);
         Ressource ressource = this.repository.trouverParId(id);
 
-        if (ressource == null) throw new ExceptionRessourceIntrouvable(id);
+        if (ressource == null){
+            log.warn("Echec de la suppression de la ressource (id): {}, id introuvable dans la base", id);
+            throw new ExceptionRessourceIntrouvable("La ressource est introuvable", id);
+        }
 
         this.repository.supprimer(id);
+        log.info("Suppression de la ressource: {}", ressource);
         return ressource;
     }
 
     @Override
     public void modifier(int id, String nom, String url, TypeRessource type) {
+        log.debug("Demande de modification de la ressource ID {}: nom='{}', url='{}', type='{}", id, nom, url, type);
         Ressource ressource = this.repository.trouverParId(id);
 
-        if (ressource == null) throw new ExceptionRessourceIntrouvable(id);
+        if (ressource == null){
+            log.warn("Echec de la modification. L'id {} est introuvable dans la base", id);
+            throw new ExceptionRessourceIntrouvable("La ressource est introuvable", id);
+        }
 
         if (nom != null && !nom.isBlank()) ressource.setNom(nom);
 
@@ -60,6 +87,7 @@ public class ServiceRessource implements IServiceRessource {
         if (type != null ) ressource.setType(type);
 
         this.repository.sauvegarder(ressource);
+        log.info("Modification de la ressource ID {}", id);
 
     }
 }
