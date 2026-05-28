21478614e6aa4d9d49b42c8569d848d3da0b8b81
Noémie <noemiechhun@gmail.com>
2026-05-28 18:08:59 +0200

    Refonte du front avec bootstrap vue next

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="text-white text-center py-4 mt-auto shadow-lg" style="background-color: #a17c5b;">
+        <BContainer>
+            
+            <p class="mb-2 small">
+                Développé par <strong>Anthia, Noémie, Bryan, Gabriel et Iraihane</strong>
+            </p>
+
+            <p class="mb-0 small opacity-75">
+                &copy; 2026 OWLearning. Tous droits réservés.
+            </p>
+        </BContainer>
+    </footer>
+</template>
+
+<script setup>
+</script>
+
+<style lang="css" scoped>
+</style>
